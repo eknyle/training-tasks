de4faddc67b41dc35c556d064097fb6757f4775e
elena.lazareva <eknyle@yandex.ru>
2024-01-08 17:39:11 +0300

    added an example for analysis

--- memoized-fibonacci/script.js
+++ memoized-fibonacci/script.js
@@ -16,6 +16,20 @@ For this particular Kata we want to implement the memoization solution. This wil
 The trick of the memoized version is that we will keep a cache data structure (most likely an associative array) where we will store the Fibonacci numbers as we calculate them. When a Fibonacci number is calculated, we first look it up in the cache, if it's not there, we calculate it and put it in the cache, otherwise we returned the cached number.
 
 Refactor the function into a recursive Fibonacci function that using a memoized data structure avoids the deficiencies of tree recursion. Can you make it so the memoization cache is private to this function?
+#intrestingSample
+var memo = function(f) {
+  var cache = {};
+  return function(n) {
+    if(!cache[n]) cache[n] = f(n);
+    return cache[n];
+  }
+};
+var fibonacci = memo(function(n) {
+    if(n==0 || n == 1)
+        return n;
+    return fibonacci(n-1) + fibonacci(n-2);
+});
+
 */
 var map=new Map();
 
